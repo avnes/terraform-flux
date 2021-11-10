26bcb18ceab28fdad926c90cc140e70a9e38ed4e
Audun Nes <audun.nes@gmail.com>
2021-11-10 20:03:32 +0100

    Do not version lock the source module

--- versions.tf
+++ versions.tf
@@ -4,23 +4,18 @@ terraform {
   required_providers {
     github = {
       source = "integrations/github"
-      version = ">= 4.16.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.5.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.11.3"
     }
     flux = {
       source  = "fluxcd/flux"
-      version = ">= 0.3.1"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "3.1.0"
     }
 
   }
